96bf04490df8065f4d41d735eaa9807edcf9648b
Lorenzo Gomez <lbg65@scarletmail.rutgers.edu>
2019-07-03 14:46:54 -0400

    Generating environment

--- GridWorld.py
+++ GridWorld.py
@@ -1,11 +1,16 @@
-from fractions import *
+from random import random
 
+BLOCKED = 1
+UNBLOCKED = 0
 
-def get_blocked_probability(p):
-    
+def is_cell_blocked(p):
+    random_num = random()
+    if random_num<=p:
+        return True
+    else:
+        return False
 
-
-def generate_grid(dim):
+def generate_grid(dim, p):
     grid = []
     x = 0
     y = 0
@@ -13,8 +18,22 @@ def generate_grid(dim):
         temp = []
         y = 0
         while(y<dim):
-            temp.append(0)
+            if( ((x!=0 or y!=0) and (x!=(dim-1) or (y!=dim-1)))  and is_cell_blocked(p) ):
+                temp.append(BLOCKED)
+            else:
+                temp.append(UNBLOCKED)
             y += 1
         grid.append(temp)
         x += 1
     return grid
+
+
+def print_grid(grid):
+    """
+    ALWAYS call print_grid to print ANY grid. DO NOT call python's built-in print().
+    """
+    formatted_grid = ""
+    for row in grid:
+        formatted_grid += str(row)
+        formatted_grid += "\n"
+    print(formatted_grid)
