a98a74c025ac0bc8d9d5a9d194bfdb83dd75aa30
Lorenzo Gomez <lbg65@scarletmail.rutgers.edu>
2019-07-05 12:59:50 -0400

    Providing the agent with functions to make it intelligent

--- GridWorld.py
+++ GridWorld.py
@@ -1,14 +1,86 @@
 from random import random
-
+import math
 BLOCKED = 1
 UNBLOCKED = 0
 
+def euclidean_distance(x1,y1,x2,y2):
+    return math.sqrt(pow((x1 - x2),2) + pow((y1 - y2), 2))
+
+def manhattan_distance(x1,y1,x2,y2):
+    return abs( x1 - x2 ) + abs(y1 - y2)
+
+
+def chebyshev_distance(x1,y1,x2,y2):
+    """
+    This function is also known as chess distance. That means how many moves it'll
+    take the king to move from (x1,y1) to (x2,y2), if the grid were a chess board.
+    """
+    return max(abs((x1) - (x2) ), abs(y1 - y2))
+
+def move_agent_north(grid, agent_position):
+    x_agent_position = agent_position[0]
+    y_agent_position = agent_position[1]
+    grid[x_agent_position][y_agent_position], grid[x_agent_position-1][y_agent_position]  = grid[x_agent_position- 1], grid[y_agent_position], grid[x_agent_position][y_agent_position]
+    print_grid(grid)
+
+def move_agent_south(grid, agent_position):
+    x_agent_position = agent_position[0]
+    y_agent_position = agent_position[1]
+    grid[x_agent_position][y_agent_position], grid[x_agent_position+1][y_agent_position]  = grid[x_agent_position+ 1][y_agent_position], grid[x_agent_position][y_agent_position]
+    print_grid(grid)
+
+def move_agent_east(grid, agent_position):
+    x_agent_position = agent_position[0]
+    y_agent_position = agent_position[1]
+    grid[x_agent_position][y_agent_position], grid[x_agent_position][y_agent_position+1]  = grid[x_agent_position][y_agent_position+1], grid[x_agent_position][y_agent_position]
+    print_grid(grid)
+
+
+def move_agent_west(grid, agent_position):
+    x_agent_position = agent_position[0]
+    y_agent_position = agent_position[1]
+    grid[x_agent_position][y_agent_position], grid[x_agent_position][y_agent_position-1]  = grid[x_agent_position][y_agent_position-1], grid[x_agent_position][y_agent_position]
+    print_grid(grid)
+
+def look_and_tell_south_cell(grid,agent_position):
+    """
+    Looks and tell if the south cell, relative to agent_position, is blocked or not.
+    If it is unblocked, this function returns True. If it is blocked, it returns false.
+    """
+    x_agent_position = agent_position[0]
+    y_agent_position = agent_position[1]
+    return grid[x_agent_position+1][y_agent_position]==UNBLOCKED
+
+def look_and_tell_north_cell(grid,agent_position):
+    """
+    Looks and tell if the north cell, relative to agent_position, is blocked or not.
+    If it is unblocked, this function returns True. If it is blocked, it returns false.
+    """
+    x_agent_position = agent_position[0]
+    y_agent_position = agent_position[1]
+    return grid[x_agent_position-1][y_agent_position]==UNBLOCKED
+
+def look_and_tell_east_cell(grid,agent_position):
+    """
+    Looks and tell if the east cell, relative to agent_position, is blocked or not.
+    If it is unblocked, this function returns True. If it is blocked, it returns false.
+    """
+    x_agent_position = agent_position[0]
+    y_agent_position = agent_position[1]
+    return grid[x_agent_position][y_agent_position+1]==UNBLOCKED
+
+def look_and_tell_west_cell(grid,agent_position):
+    """
+    Looks and tell if the west cell, relative to agent_position, is blocked or not.
+    If it is unblocked, this function returns True. If it is blocked, it returns false.
+    """
+    x_agent_position = agent_position[0]
+    y_agent_position = agent_position[1]
+    return grid[x_agent_position][y_agent_position-1]==UNBLOCKED
+
 def is_cell_blocked(p):
     random_num = random()
-    if random_num<=p:
-        return True
-    else:
-        return False
+    return random_num<=p
 
 def generate_grid(dim, p):
     grid = []
@@ -26,6 +98,8 @@ def generate_grid(dim, p):
             y += 1
         grid.append(temp)
         x += 1
+    grid[0][0] = 'S'
+    grid[dim-1][dim-1] = 'G'
     return grid
 
 
